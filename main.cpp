a761bb194903716aa51581958469414c622f49fe
Error001001 <98566029+ERROR001001@users.noreply.github.com>
2022-05-11 04:16:30 +0000

    better

--- main.cpp
+++ main.cpp
@@ -4,33 +4,18 @@
 #include <string>
 #include <time.h>
 #include <unistd.h>
+#include "chooseyourownadventure.h"
 
 using namespace std;
 
-float textspeed, ansnum;
-string ans;
-
-void slep(float time) { usleep(time * 1000000); }
-
-void in(string type, string question) {
-  cout << question << type;
-  getline(cin, ans);
-}
-
-void innum(string type, string question) {
-  cout << question << type;
-  cin >> ansnum;
-}
-
-void out(string print) { cout << print; }
-
 int main() {
-  innum(": ", "Textspeed:\n1. Slow\n2. Medium\n3. Fast\n4. Instant\nChoice");
+  ChooseYourOwnAdventure s;
+  s.innum(": ", "Textspeed:\n1. Slow\n2. Medium\n3. Fast\n4. Instant\nChoice");
   //change this if statement to adjust time
-  if (ansnum == 1 || ansnum == 2 || ansnum == 3) {
-    textspeed = 5 - (ansnum * 1.5);
-  } else if (ansnum == 4) {
-    textspeed = 0;
+  if (s.ansnum == 1 || s.ansnum == 2 || s.ansnum == 3) {
+    s.textspeed = 5 - (s.ansnum * 1.5);
+  } else if (s.ansnum == 4) {
+    s.textspeed = 0;
   } else {
     cout << "Invalid input";
     exit(1);
